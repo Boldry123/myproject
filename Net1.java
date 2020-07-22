af25542a167714a66cd46a96cc00a961e545fde5
Boldry123 <42084238+Boldry123@users.noreply.github.com>
2020-07-22 20:27:03 +0800

    Add files via upload

---
+++ Net1.java
@@ -0,0 +1,31 @@
+package fish_catch;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+
+
+public class Net1 {
+	int x,y;
+	int width,height;
+	BufferedImage netImage;
+	boolean show;
+	public Net1(){
+		try {
+			netImage=ImageIO.read(new File("src/fish/net09.png"));
+			width=netImage.getWidth();
+			height=netImage.getHeight();
+			x=0;y=0;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public void moveTo(int x,int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+}
